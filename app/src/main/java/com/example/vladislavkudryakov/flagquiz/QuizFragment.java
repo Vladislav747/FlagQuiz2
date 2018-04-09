11dd6c8fccce487b95b25ef0c89ae9a1134cd76a
Melo <zippo585@gmail.com>
2018-04-10 02:39:14 +0500

    Layouts
    including layouts of land scape

---
+++ app/src/main/java/com/example/vladislavkudryakov/flagquiz/QuizFragment.java
@@ -0,0 +1,10 @@
+package com.example.vladislavkudryakov.flagquiz;
+
+import android.app.Fragment;
+
+/**
+ * Created by Melo on 09.04.2018.
+ */
+
+public class QuizFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/vladislavkudryakov/flagquiz/SettingsFragment.java
@@ -0,0 +1,11 @@
+package com.example.vladislavkudryakov.flagquiz;
+
+import android.app.Fragment;
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by Melo on 09.04.2018.
+ */
+
+public class SettingsFragment extends PreferenceFragment {
+}
