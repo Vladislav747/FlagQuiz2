b49ef3fc5e0d55da6d8d36bad58514c385111aec
Melo <zippo585@gmail.com>
2018-04-13 03:40:34 +0500

    Layouts
    including layouts of land scape

--- app/src/main/java/com/example/vladislavkudryakov/flagquiz/SettingsFragment.java
+++ app/src/main/java/com/example/vladislavkudryakov/flagquiz/SettingsFragment.java
@@ -1,6 +1,7 @@
 package com.example.vladislavkudryakov.flagquiz;
 
-import android.app.Fragment;
+
+import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
 /**
@@ -8,4 +9,10 @@ import android.preference.PreferenceFragment;
  */
 
 public class SettingsFragment extends PreferenceFragment {
+    // creates preferences GUI from preferences.xml file in res/xml
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences); // load from XML
+    }
 }
